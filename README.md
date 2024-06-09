dd28f9fc97cd0c269cf32bbbd421c9165a59519e
Konstantin Chepurin <chepurin.konst@yandex.ru>
2024-06-09 21:43:02 +0700

    add: readme, create cards, delete cards

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Проектная работа Mesto
+# SPRINT_5 Проектная работа Mesto
+
+[Ссылка на репозиторий](https://github.com/CheKonstantin/mesto-project-ff.git)
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardElementContent = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const listCards = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(arr, parent) {
+  arr.forEach((element) => {
+    const cardTemplate = cardElementContent
+        .querySelector('.card')
+        .cloneNode(true),
+      cardImage = cardTemplate.querySelector('.card__image'),
+      cardBtnDelete = cardTemplate.querySelector('.card__delete-button'),
+      cardTitle = cardTemplate.querySelector('.card__title');
+    cardImage.src = element.link;
+    cardImage.alt = element.name;
+    cardTitle.textContent = element.name;
+    parent.append(cardTemplate);
+    cardBtnDelete.addEventListener('click', (e) => {
+      deleteCard(e.target);
+    });
+  });
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(event) {
+  event.parentNode.remove();
+}
 // @todo: Вывести карточки на страницу
+createCard(initialCards, listCards);
+
