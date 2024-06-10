85d913d58545672a4dc09ceff964752f186c30ca
Konstantin Chepurin <chepurin.konst@yandex.ru>
2024-06-10 22:26:54 +0700

    fix after review №1

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,63 @@
-// @todo: Темплейт карточки
 const cardElementContent = document.querySelector('#card-template').content;
-// @todo: DOM узлы
 const listCards = document.querySelector('.places__list');
-// @todo: Функция создания карточки
-function createCard(arr, parent) {
-  arr.forEach((element) => {
-    const cardTemplate = cardElementContent
-        .querySelector('.card')
-        .cloneNode(true),
-      cardImage = cardTemplate.querySelector('.card__image'),
-      cardBtnDelete = cardTemplate.querySelector('.card__delete-button'),
-      cardTitle = cardTemplate.querySelector('.card__title');
-    cardImage.src = element.link;
-    cardImage.alt = element.name;
-    cardTitle.textContent = element.name;
-    parent.append(cardTemplate);
-    cardBtnDelete.addEventListener('click', (e) => {
-      deleteCard(e.target);
-    });
+
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = cardElementContent
+    .querySelector('.card')
+    .cloneNode(true);
+
+  const cardImage = cardTemplate.querySelector('.card__image');
+  const cardBtnDelete = cardTemplate.querySelector('.card__delete-button');
+  const cardTitle = cardTemplate.querySelector('.card__title');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  cardBtnDelete.addEventListener('click', () => {
+    deleteCallback(cardTemplate);
   });
+
+  return cardTemplate;
 }
-// @todo: Функция удаления карточки
-function deleteCard(event) {
-  event.parentNode.remove();
+
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
-// @todo: Вывести карточки на страницу
-createCard(initialCards, listCards);
+
+const initialCards = [
+  // Список ваших начальных карточек
+];
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  listCards.append(cardElement);
+});
+// const cardElementContent = document.querySelector('#card-template').content;
+// // @todo: DOM узлы
+// const listCards = document.querySelector('.places__list');
+// // @todo: Функция создания карточки
+// function createCard(arr, parent) {
+//   arr.forEach((element) => {
+//     const cardTemplate = cardElementContent
+//         .querySelector('.card')
+//         .cloneNode(true),
+//       cardImage = cardTemplate.querySelector('.card__image'),
+//       cardBtnDelete = cardTemplate.querySelector('.card__delete-button'),
+//       cardTitle = cardTemplate.querySelector('.card__title');
+//     cardImage.src = element.link;
+//     cardImage.alt = element.name;
+//     cardTitle.textContent = element.name;
+//     parent.append(cardTemplate);
+//     cardBtnDelete.addEventListener('click', (e) => {
+//       deleteCard(e.target);
+//     });
+//   });
+// }
+// // @todo: Функция удаления карточки
+// function deleteCard(event) {
+//   event.parentNode.remove();
+// }
+// // @todo: Вывести карточки на страницу
+// createCard(initialCards, listCards);
 
