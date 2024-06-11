fc504f6d196b948cb233912b33ae5466a0a5745d
Konstantin <k.chepurin@anketolog.ru>
2024-06-11 09:04:28 +0700

    fix after review 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,62 +2,32 @@ const cardElementContent = document.querySelector('#card-template').content;
 const listCards = document.querySelector('.places__list');
 
 function createCard(cardData, deleteCallback) {
-  const cardTemplate = cardElementContent
+  const cardElement = cardElementContent
     .querySelector('.card')
     .cloneNode(true);
 
-  const cardImage = cardTemplate.querySelector('.card__image');
-  const cardBtnDelete = cardTemplate.querySelector('.card__delete-button');
-  const cardTitle = cardTemplate.querySelector('.card__title');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardBtnDelete = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
 
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
 
   cardBtnDelete.addEventListener('click', () => {
-    deleteCallback(cardTemplate);
+    deleteCallback(cardElement);
   });
 
-  return cardTemplate;
+  return cardElement;
 }
 
 function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-const initialCards = [
-  // Список ваших начальных карточек
-];
-
 initialCards.forEach((cardData) => {
   const cardElement = createCard(cardData, deleteCard);
   listCards.append(cardElement);
 });
-// const cardElementContent = document.querySelector('#card-template').content;
-// // @todo: DOM узлы
-// const listCards = document.querySelector('.places__list');
-// // @todo: Функция создания карточки
-// function createCard(arr, parent) {
-//   arr.forEach((element) => {
-//     const cardTemplate = cardElementContent
-//         .querySelector('.card')
-//         .cloneNode(true),
-//       cardImage = cardTemplate.querySelector('.card__image'),
-//       cardBtnDelete = cardTemplate.querySelector('.card__delete-button'),
-//       cardTitle = cardTemplate.querySelector('.card__title');
-//     cardImage.src = element.link;
-//     cardImage.alt = element.name;
-//     cardTitle.textContent = element.name;
-//     parent.append(cardTemplate);
-//     cardBtnDelete.addEventListener('click', (e) => {
-//       deleteCard(e.target);
-//     });
-//   });
-// }
-// // @todo: Функция удаления карточки
-// function deleteCard(event) {
-//   event.parentNode.remove();
-// }
-// // @todo: Вывести карточки на страницу
-// createCard(initialCards, listCards);
+
 
